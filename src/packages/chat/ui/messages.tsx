37af675aa435ee2da639264a8be4bf855dc971e0
Jakub Kołosowski <kuba.kolosowski@icloud.com>
2022-10-30 16:01:23 +0100

    Added top bar to every main route component

--- src/packages/chat/ui/messages.tsx
+++ src/packages/chat/ui/messages.tsx
@@ -1,7 +1,15 @@
 import { ReactElement } from "react";
 
+import { TopBar } from "../../../common/components";
+
 const Messages = (): ReactElement => {
-  return <span>Messages</span>;
+  return (
+    <section>
+      <TopBar role="candidate" name="Jakub Kołosowski">
+        <span>Messages</span>
+      </TopBar>
+    </section>
+  );
 };
 
 export default Messages;
