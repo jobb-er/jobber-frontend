d36254c1d7a94ff31d0152e138e316046cf4f014
Jakub Kołosowski <kuba.kolosowski@icloud.com>
2023-01-22 20:58:34 +0100

    Disabled email edit on profile

--- src/common/components/input/input.tsx
+++ src/common/components/input/input.tsx
@@ -33,7 +33,9 @@ const Input = ({
     {Icon ? <Icon className="absolute w-5 h-5 top-2 left-3" /> : <></>}
     <input
       className={removeDuplicateWhitespaces(
-        `bg-secondary-lightest rounded-xl h-9 placeholder:opacity-40 focus:outline-none w-full border ${
+        `bg-secondary-lightest rounded-xl h-9
+        placeholder:opacity-40 focus:outline-none
+        w-full disabled:opacity-40 border ${
           isError ? "border-error" : "border-secondary-lightest"
         } ${Icon ? "pl-10 pr-3" : "p-3"} ${additionalClassName}`,
       )}
